fafffa4a0b103305201103015625156ddb62c5cd
github username <github email address>
2019-07-05 09:01:13 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
-class BankAccount
+require "pry"
 
+class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+    
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 class Transfer
-  # your code here
+  	attr_accessor :sender, :receiver, :status, :amount
+  
+	def initialize(sender, receiver, status = "pending", amount)
+		@sender = sender
+		@receiver = receiver
+		@status = status
+		@amount = amount  
+	end
+
+	def valid?
+		self.sender.valid? && self.receiver.valid?
+	end
+
+	def execute_transaction
+		if self.valid? && @status == "pending" && @sender.balance > amount
+			@sender.balance -= @amount
+			@receiver.balance += @amount
+			@status = "complete"
+		else
+			# I don't believe this code would ever run because all instances of transfer put forth by the test passes validity above.
+			# @sender.valid? == false
+			@status = "rejected"
+			"Transaction rejected. Please check your account balance."
+		end
+	end
+
+	def reverse_transfer
+		if self.valid? && @status == "complete"
+			@sender.balance += @amount
+			@receiver.balance -= @amount
+			@status = "reversed"
+		end
+	end
 end
